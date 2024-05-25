e4c34422f3be7098cd409c27f202cc60d65138a1
XxKingFrandyxX <151718434+XxKingFrandyxX@users.noreply.github.com>
2024-05-25 23:48:18 +1000

    tank gamev3

---
+++ Assets/Scripts/menuUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuUI : MonoBehaviour
+{
+   
+    public void PlayGame()
+    {
+        Debug.Log("Play Game");
+    }
+
+  
+    public void Exit()
+    {
+        Debug.Log("Exit");
+    }
+
+    public void buttonFunction1()
+    {
+
+    }
+
+    public void buttonFunction2(int value)
+    {
+
+    }
+}
