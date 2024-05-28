a4fb1c51006b90a7fc6a1624be7c580da61aa885
XxKingFrandyxX <151718434+XxKingFrandyxX@users.noreply.github.com>
2024-05-28 12:28:01 +1000

    TankGAme homework

--- Assets/Scripts/menuUI.cs
+++ Assets/Scripts/menuUI.cs
@@ -1,28 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class menuUI : MonoBehaviour
 {
-   
-    public void PlayGame()
+    public GameObject exitMenu;
+    public TMP_Text m_HighScoresMsg;
+    public HighScores m_highScores;
+    int[]  m_Scores;
+    public void PlayGame(string sceneName)
     {
         Debug.Log("Play Game");
+
+        SceneManager.LoadScene(sceneName);
     }
 
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
   
     public void Exit()
     {
         Debug.Log("Exit");
+        Application.Quit();
     }
 
-    public void buttonFunction1()
+    public void ExitMenu(bool exiting)
     {
-
+        exitMenu.SetActive(exiting);
     }
 
-    public void buttonFunction2(int value)
+    void DisplayHighScores()
     {
+        m_Scores = m_highScores.GetScores();
 
+        string text = "";
+        for (int i = 0; i < m_Scores.Length; i++)
+        {
+            int seconds = m_Scores[i];
+            text += string.Format("{0:D2}:{1:D2}\n",
+                          (seconds / 60), (seconds % 60));
+        }
+        m_HighScoresMsg.text = text;
+    }
+
+    private void Start()
+    {
+        DisplayHighScores();
     }
 }
